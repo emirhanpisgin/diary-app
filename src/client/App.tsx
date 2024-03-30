fcc7b2745228e1a4cedaabfa49d096a9a08239fa
kryparnold <45902187+kryparnold@users.noreply.github.com>
2024-03-30 07:05:50 +0300

    implemented landing page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,27 @@
+import AddDiary from "./components/AddDiary";
+import { useDialog } from "./components/context/DialogContext";
+
 export default function App() {
-    return(
-        <div className="h-screen w-screen bg-zinc-800 grid place-items-center text-7xl text-zinc-300">
-            Hello, World!
+    const { setDialogComponent } = useDialog();
+
+    return (
+        <div>
+            <div className="text-center text-6xl py-6 font-semibold bg-zinc-600 relative">
+                Günlük
+                <div className="absolute h-full top-0 right-0 aspect-square p-5">
+                    <div className="bg-blue-500 aspect-square rounded-xl grid place-items-center hover:shadow-xl shadow-blue-500 transition-shadow cursor-pointer" onClick={() => setDialogComponent(<AddDiary />)}>
+                        <AddIcon />
+                    </div>
+                </div>
+            </div>
         </div>
     )
+}
+
+function AddIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.75rem" height="2rem" viewBox="0 0 448 512">
+            <path fill="currentColor" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32v144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32h144v144c0 17.7 14.3 32 32 32s32-14.3 32-32V288h144c17.7 0 32-14.3 32-32s-14.3-32-32-32H256z" />
+        </svg>
+    );
 }
