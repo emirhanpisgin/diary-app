461e7cc85cd37e334514b8d80e4cd98386b46533
kryparnold <45902187+kryparnold@users.noreply.github.com>
2024-04-03 17:25:45 +0300

    implemented diary preview

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,27 @@
 import AddDiary from "./components/AddDiary";
 import { useDialog } from "./components/context/DialogContext";
+import { useDiary } from "./components/context/DiaryContext";
+import DiaryCard from "./components/DiaryCard";
 
 export default function App() {
     const { setDialogComponent } = useDialog();
+    const { diaries } = useDiary();
 
     return (
-        <div>
+        <div className="flex flex-col h-full">
             <div className="text-center text-6xl py-6 font-semibold bg-zinc-600 relative">
                 Günlük
                 <div className="absolute h-full top-0 right-0 aspect-square p-5">
                     <div className="bg-blue-500 aspect-square rounded-xl grid place-items-center hover:shadow-xl shadow-blue-500 transition-shadow cursor-pointer" onClick={() => setDialogComponent(<AddDiary />)}>
                         <AddIcon />
                     </div>
+                </div>
+            </div>
+            <div className="p-5 flex-1">
+                <div className="bg-zinc-600 h-full w-full rounded-xl flex flex-wrap gap-5 p-3">
+                    {diaries.map((diary, index) => (
+                        <DiaryCard key={index} content={diary.content} date={diary.date} />
+                    ))}
                 </div>
             </div>
         </div>
